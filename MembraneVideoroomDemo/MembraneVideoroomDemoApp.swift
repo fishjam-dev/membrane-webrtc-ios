3ca688aa447ae800ab19607546ee2fe239a9ab09
Jakub Perżyło <qizot1405@gmail.com>
2022-01-17 09:34:33 +0100

    Initial Commit

---
+++ MembraneVideoroomDemo/MembraneVideoroomDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MembraneVideoroomDemoApp.swift
+//  MembraneVideoroomDemo
+//
+//  Created by Jakub Perzylo on 17/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct MembraneVideoroomDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
