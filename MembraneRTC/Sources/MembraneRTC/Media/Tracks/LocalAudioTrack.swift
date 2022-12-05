82bdc4418bd1d1adb5858673fec4ca007ba2e5eb
Angelika Serwa <angelika.serwa@swmansion.com>
2022-12-05 23:09:48 +0100

    [MV-232] Add possibility to toggle video/voice chat mode in audio session configuration (#19)
    * Add possibility to toggle video/voice chat mode in audio session configuration
    
    * Add docs
    
    * Avoid duplication

--- MembraneRTC/Sources/MembraneRTC/Media/Tracks/LocalAudioTrack.swift
+++ MembraneRTC/Sources/MembraneRTC/Media/Tracks/LocalAudioTrack.swift
@@ -55,15 +55,40 @@ public class LocalAudioTrack: AudioTrack, LocalTrack {
         return track.trackId
     }
 
-    private func configure(setActive: Bool) {
+    private func withAudioSession(callback: ((RTCAudioSession) throws -> Void)) {
         let audioSession = RTCAudioSession.sharedInstance()
         audioSession.lockForConfiguration()
         defer { audioSession.unlockForConfiguration() }
 
         do {
-            try audioSession.setConfiguration(config, active: setActive)
+            try callback(audioSession)
         } catch {
             sdkLogger.error("Failed to set configuration for audio session")
         }
+    }
+
+    private func configure(setActive: Bool) {
+        withAudioSession { audioSession in
+            try audioSession.setConfiguration(config, active: setActive)
+        }
+    }
+
+    private func setMode(mode: String) {
+        withAudioSession { audioSession in
+            config.mode = mode
+            try audioSession.setConfiguration(config)
+        }
+    }
+
+    /// Sets AVAudioSession configuration mode to voice chat
+    /// (https://developer.apple.com/documentation/avfaudio/avaudiosession/mode/1616455-voicechat)
+    public func setVoiceChatMode() {
+        setMode(mode: AVAudioSession.Mode.voiceChat.rawValue)
+    }
+
+    /// Sets AVAudioSession configuration mode to video chat
+    /// (https://developer.apple.com/documentation/avfaudio/avaudiosession/mode/1616590-videochat)
+    public func setVideoChatMode() {
+        setMode(mode: AVAudioSession.Mode.videoChat.rawValue)
     }
 }
