af9d06f324d1b601099f12324c9c4b3204c1feb1
Angelika Serwa <angelika.serwa@swmansion.com>
2023-04-26 11:54:35 +0200

    Fix encoding changes

--- MembraneRTC/Sources/MembraneRTC/Types/EncodingReason.swift
+++ MembraneRTC/Sources/MembraneRTC/Types/EncodingReason.swift
@@ -1,11 +1,11 @@
 /**
  * Type describing possible reasons of currently selected encoding.
  * - other - the exact reason couldn't be determined
- * - encodingInactive - previously selected encoding became inactive
- * - lowBandwidth - there is no longer enough bandwidth to maintain previously selected encoding
+ * - encoding_inactive - previously selected encoding became inactive
+ * - low_bandwidth - there is no longer enough bandwidth to maintain previously selected encoding
  */
 public enum EncodingReason: String {
     case other
-    case encodingInactive
-    case lowBandwidth
+    case encoding_inactive
+    case low_bandwidth
 }
