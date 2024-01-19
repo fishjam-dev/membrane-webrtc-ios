0f4e27b86de92665c04e3d6e701f0aa01232bb65
Karol Kąkol <kakol.karol00@gmail.com>
2024-01-19 11:18:35 +0100

    add initializer to TrackData

--- Sources/MembraneRTC/Types/TrackData.swift
+++ Sources/MembraneRTC/Types/TrackData.swift
@@ -1,4 +1,9 @@
 public struct TrackData: Codable{
     let metadata: Metadata
     let simulcastConfig: SimulcastConfig?
+    
+    public init (metadata : Metadata, simulcastConfig : SimulcastConfig? = nil) {
+        self.metadata = metadata
+        self.simulcastConfig = simulcastConfig
+    }
 }
