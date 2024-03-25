80e9585d08d4bf6c23c509c55971ee44170dd976
Karol Kąkol <kakol.karol00@gmail.com>
2024-03-25 15:10:01 +0100

    add initialized to decode nullable metadata in TrackData.swift

--- Sources/MembraneRTC/Types/TrackData.swift
+++ Sources/MembraneRTC/Types/TrackData.swift
@@ -1,12 +1,18 @@
-public struct TrackData: Codable{
+public struct TrackData: Codable {
     public let metadata: Metadata
     public let simulcastConfig: SimulcastConfig?
-    
-    public init (metadata : Metadata, simulcastConfig : SimulcastConfig? = nil) {
+
+    public init(metadata: Metadata, simulcastConfig: SimulcastConfig? = nil) {
         self.metadata = metadata
         self.simulcastConfig = simulcastConfig
     }
-    
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.metadata = try container.decodeIfPresent(Metadata.self, forKey: .metadata) ?? Metadata()
+        self.simulcastConfig = try container.decodeIfPresent(SimulcastConfig.self, forKey: .simulcastConfig)
+    }
+
     func copyWith(metadata: Metadata? = nil, simulcastConfig: SimulcastConfig? = nil) -> TrackData {
         return TrackData(
             metadata: metadata ?? self.metadata,
